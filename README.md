f141de253d37310963ef03e263058a6b5d9afed5
Jose Castro <jacastro023@gmail.com>
2019-12-22 23:11:35 -0600

    added info to readme.md file

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # Day-Planner
 creating a planner that saves on local storage
+
+## Description
+
+* The point of this assignment was to create a day planner which would change colors by itself depending on the time of the day.
+
+* for example if the time had already passed that row would be a certain color while upcoming times would be a different color.
+
+* In this assignment I tried to use all jquery without having to write my stuff out in the html file.
+
+* I also made use of moment.js to be able to handle the time part of this planner.
+
+## Functionality
+
+* Whatever the user had typed for the certain textarea, upon refreshing the page, the text is supposed to stay there because of the localstorage.
+
+* without having to change the page manually, the time had to update to the current time.
+
+* The Textareas also had to be multiple colors according to what time they correspond.
+
+* each textarea has its own save button which upon clicking it it would send the content to the localstorage.
+
+## Reocurring-Errors
+
+* Making the moment.js match the time i wrote in as an array was tough work since it seams that moment.js works on military time.
+
+* Certain things would not console.log and so I had no idea an what i had to work on in order to keep moving forward.
+
+* This was a lot of guess work and moving small things around.
+
+* When to call the functions and which functions had to work as soon as the page was opened was another thing entirely, I had to keep going back and forth between the code and the page to check what was changing.
+
+* It seems as if one text area would ocuply all the local storage, and upon recalling the content it would apply to all the textareas.
+
+## Planned-Improvements
+
+* I am not entirely sure if I linked the buttons to the different textareas.
+
+* I have to separate the different textarea's localstorage so that they do no overwrite each other.
--- script.js
+++ script.js
@@ -4,16 +4,17 @@ var date = moment().hour()
 
 
 
+
 // var time = ["9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM"]
 
 // var image = images / locks.jpg
 var time = [8, 10, 11, 12, 13, 14, 15, 16, 17]
-// for (i = 0; i < time.length; i++) {
+// made a function that used all separate values of time variable
 time.forEach(function (id, i) {
-    // console.log(time[i])
+
     var hours = time[i]
     var timeValue;
-
+    // changing the value of time to hours instead of military time
     if (hours > 0 && hours <= 12) {
         timeValue = "" + hours;
     } else if (hours > 12) {
@@ -25,38 +26,34 @@ time.forEach(function (id, i) {
     // console.log(timeValue)
 
     // creating a row with 3 colums the first one taking an index from the variable time, the second containing a textare and the third containing a button.
-    var newrow = $("<tr><Td class='hour'>" + timeValue + "</td><td><textarea id=" + [i] + "></textarea></td><td><button id=" + [i] + ">a</button></td></tr>")
+    var newrow = $("<tr><Td class='hour'>" + timeValue + "</td><td><textarea id= " + [i] + "></textarea></td><td><button class= " + [i] + ">a</button></td></tr>")
     $(".container").append(newrow)
 
-    $("button").on("click", function () {
+    $("." + [i]).on("click", function () {
         event.preventDefault();
-        event.stopPropagation();
-        console.log($("textarea " + [i]).data("id"));
-
-        var user = $("textarea").val();
-        localStorage.setItem("user", user);
-    })
-
-    // function render() {
-    //     var user = $("textarea").val().trim();
-    //     $("textarea").text(user)
-    //     console.log(user);
-    //     localStorage.setItem("user", user);
-    //     console.log(localStorage.setItem("user", 7user)));
-
-    // }
-    console.log(time[i]);
-    console.log(date);
-
-
-
-    $(document).ready(function () {
+        console.log($("#" + i).val());
 
         $("#" + i).each(function () {
-            console.log("#" + i)
-            console.log(time[i] < date)
-            console.log(time[i] > date)
+            var user = $("#" + i).val();
+            localStorage.setItem("user", user);
+            console.log(user);
 
+            $("#" + i).text(localStorage.user[i])
+            console.log($("#" + i).text(localStorage.user));
+
+
+        })
+    })
+
+    $(document).ready(function () {
+        $("#" + i).text(localStorage.user)
+        $("#" + i).each(function () {
+            // checked if the data i was looking for was correct
+            // console.log("#" + i)
+            // console.log(time[i] < date)
+            // console.log(time[i] > date)
+
+            // created the color deciding statement
             if (time[i] < date) {
                 $("#" + i).attr("class", "past")
             } else if (time[i] > date) {
@@ -70,38 +67,6 @@ time.forEach(function (id, i) {
 })
 
 
-
-// $("textarea").attr("class", "past")
-// $("textarea").attr("class", "present")
-// $("textarea").attr("class", "future")
+// adding classes to the button
 $("button").attr("class", "saveBtn time-block")
-
-// {/* <img src='images/locks.jpg'></img> */ }
-
-// $("button").on("click", function () {
-//     event.preventDefault();
-//     var user = $("textarea").val();
-//     localStorage.setItem("user", user);
-// })
-
-function render() {
-    var user = localStorage.getItem("user");
-    $("textarea").text(user)
-}
-render();
-
-// var hours = time[i]
-// var timeValue;
-
-// if (hours > 0 && hours <= 12) {
-//     timeValue = "" + hours;
-// } else if (hours > 12) {
-//     timeValue = "" + (hours - 12);
-// } else if (hours == 0) {
-//     timeValue = "12";
-// }
-// timeValue += (hours >= 12) ? " P.M." : " A.M.";
-// console.log(timeValue)
-
-
 
